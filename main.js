795adaa4baa99948dc1d960c625b3fcca0bbac5c
MiguelBrito1086 <mbrito1086@gmail.com>
2017-08-11 14:04:32 -0400

    trying to populate word

--- main.js
+++ main.js
@@ -1,65 +1,82 @@
-const express = require('express')
-const mustacheExpress = require('mustache-express')
-const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
-const fs = require('fs')
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const fs = require('fs');
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
 
-const app = express()
+const app = express();
 
-app.engine('mst', mustacheExpress())
-app.set('views', './views')
-app.set('view engine', 'mst')
+app.engine('mst', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mst');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(expressValidator())
-
-const randomWord = words[Math.floor(Math.random() * words.length)]
-
-const invisWord = []
-
-const PLACEHOLDER = "_"
-
-// const guesses = []
-//
-// const hangman = (randomWord, guesses)
-//   randomWord
-//     .split('')
-//     .map(letter => guesses.includes(letter) ? letter : PLACEHOLDER)
-//     .join('')
-
-
-for (let i = 0; i < randomWord.length; i++) {
-  invisWord.push('_')
-}
-
-// randomWord.forEach( PLACEHOLDER => {
-//   invisWord.push(PLACEHOLDER)
-// })
-
-console.log(randomWord);
-console.log(invisWord);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(expressValidator());
 
 
 
+const randomWord = words[Math.floor(Math.random() * words.length)];
 
 
+const PLACEHOLDER = "_";
+
+const letterGuess = [];
+
+const attempts = 8;
+
+
+const hangman = (randomWord, guesses) => {
+  randomWord
+    .split('')
+    .map(letter => guesses.includes(letter) ? letter : PLACEHOLDER)
+    .join(' ');}
+
+const invisWord = hangman(randomWord);
+
+invisWord.push(hangman(randomWord, letterGuess));
 
 app.get('/', (req, resp) => {
 
-  resp.render('home',  { invisWord });
+
+  resp.render('home',  { invisWord, attempts });
 })
 
-app.post('/', (req, resp) => {
-  
-  resp.redirect('/')
-})
 
+
+app.post('/letters', (req, resp) => {
+
+  const guess = req.body.guess
+
+  req
+    .checkBody("guess", "You must guess a single letter")
+    .notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    resp.send("You must guess a single letter")
+  } else {
+    letterGuess.push(guess)
+  }
+
+  for (var i = 0; i < randomWord.length; i++) {
+    if (randomWord[i] === guess) {
+      invisWord[i] = guess;
+    } else guessed.push(guess)
+  }
+
+console.log(guessed);
+
+  // req.session.letterGuess = letterGuess
+  // resp.render('home', { invisWord })
+  resp.redirect('/');
+})
 
 
 
@@ -69,5 +86,5 @@ app.post('/', (req, resp) => {
 
 
 app.listen(3000, () => {
-  console.log("Magic is happening on port 3000")
-});
+  console.log("Magic is happening on port 3000");
+})
